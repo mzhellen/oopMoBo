d170cea9c0089c44cbeda0d0ac4048e5aafe2294
mzhellen <mizuki.costa@gmail.com>
2025-05-19 13:36:56 -0300

    Commit inicial, estado base

---
+++ mo_bo/src/test/java/com/poo/mo_bo/MoBoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poo.mo_bo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoBoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
