df4673db49af8f5113dccbcca02a1879abc8f13a
Heloiza Graziele <grazieleamaro39@gmail.com>
2025-06-04 18:01:26 -0300

    Desenvolvimento da Segurança na API

---
+++ mo_bo/src/main/java/com/poo/mo_bo/auth/CustomAuthFilter.java
@@ -0,0 +1,43 @@
+package com.poo.mo_bo.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthFilter extends OncePerRequestFilter {
+
+    private final AuthService authService;
+
+    public CustomAuthFilter(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String hash = request.getHeader("Authorization");
+
+        String path = request.getRequestURI();
+        if (path.contains("/login") || path.contains("/register")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (hash == null || !authService.isValid(hash)) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Acesso negado");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
+
